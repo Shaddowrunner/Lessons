e45a5714d6cf8d23ecbe85e1997598ed79b3dd29
Slava <stalker.f.d.h@gmail.com>
2017-04-22 18:28:05 +0300

    Delete out dir, add gitignore, modify other classes

--- src/homeworks/Calculator.java
+++ src/homeworks/Calculator.java
@@ -17,33 +17,19 @@ public class Calculator {
         return first + second;
     }
 
-    public int getPlus() {
-        return plus();
-    }
-
     public int minus() {
         return first - second;
-    }
-
-    public int getMinus() {
-        return minus();
     }
 
     public int multiply() {
         return first * second;
     }
 
-    public int getMultiply() {
-        return plus();
-    }
 
     public int degree() {
         return first * second;
     }
 
-    public int getDegree() {
-        return degree();
-    }
 
     public int getFirst() {
         return first;
@@ -62,22 +48,45 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        Calculator sum = new Calculator(0, 2);
-        sum.setFirst(8);
-        sum.setSecond(2);
-        System.out.println(sum.getPlus());
+      /*
+
+        Before
+
+      Calculator calculator = new Calculator(0, 2);
+        calculator.setFirst(8);
+        calculator.setSecond(2);
+        System.out.println(calculator.plus());
         Calculator minus = new Calculator(0, 3);
         minus.setFirst(15);
         minus.setSecond(3);
-        System.out.println(minus.getMinus());
+        System.out.println(minus.minus());
         Calculator multi = new Calculator(1, 4);
         multi.setFirst(4);
         multi.setSecond(6);
-        System.out.println(multi.getMultiply());
+        System.out.println(multi.multiply());
         Calculator degree = new Calculator(2, 6);
         degree.setFirst(128);
         degree.setSecond(32);
-        System.out.println(degree.getDegree());
+        System.out.println(degree.degree());*/
+
+        Calculator calculator = new Calculator(8, 2);
+
+        System.out.println(calculator.plus());
+
+        calculator.setFirst(15);
+        calculator.setSecond(3);
+
+        System.out.println(calculator.minus());
+
+        calculator.setFirst(4);
+        calculator.setSecond(6);
+
+        System.out.println(calculator.multiply());
+
+        calculator.setFirst(128);
+        calculator.setSecond(32);
+
+        System.out.println(calculator.degree());
     }
 
 }
---
+++ src/homeworks/StringHome.java
@@ -0,0 +1,8 @@
+package homeworks;
+
+/**
+ * Created by Shaddowrunner on 22.04.2017.
+ */
+public class StringHome {
+    String hello = "Hello World";
+}
